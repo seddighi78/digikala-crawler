edc3118c955f5bb7d036ab88281fbb665b546278
Alireza Seddighi <seddighi.alireza78@gmail.com>
2021-12-05 18:01:28 +0330

    Add sqlite database

--- digikala/pipelines.py
+++ digikala/pipelines.py
@@ -1,13 +1,49 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import sqlite3
+import json
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+class SqlitePipeline:
 
+    def __init__(self, name):
+        self.connection = sqlite3.connect(name)
+        self.cursor = self.connection.cursor()
+        self.create_products_table()
 
-class DigikalaPipeline:
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            name=crawler.settings.get('SQLITE_DATABASE_NAME')
+        )
+
+    def create_products_table(self):
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS products(
+            id real primary key,
+            title text null,
+            category varchar null,
+            sizes text null,
+            colors text null,
+            pure_price real null,
+            image_urls text null                   
+        )
+        """)
+
     def process_item(self, item, spider):
+        self.insert_item(item)
+
         return item
+
+    def insert_item(self, item):
+        self.cursor.execute("""INSERT OR IGNORE INTO products VALUES(?,?,?,?,?,?,?)""", (
+            item['id'],
+            item['title'],
+            item['category'],
+            json.dumps(item['sizes']),
+            json.dumps(item['colors']),
+            item['pure_price'],
+            item['image_urls'][0],
+        ))
+        self.connection.commit()
+
+    def close_spider(self, spider):
+        self.connection.close()
--- digikala/settings.py
+++ digikala/settings.py
@@ -12,9 +12,8 @@ BOT_NAME = 'digikala'
 SPIDER_MODULES = ['digikala.spiders']
 NEWSPIDER_MODULE = 'digikala.spiders'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'digikala (+http://www.yourdomain.com)'
+# USER_AGENT = 'digikala (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -27,64 +26,67 @@ CONCURRENT_REQUESTS = 32
 # See also autothrottle settings and docs
 DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'digikala.middlewares.DigikalaSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'digikala.middlewares.DigikalaDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'scrapy.pipelines.images.ImagesPipeline': 1
+    'scrapy.pipelines.images.ImagesPipeline': 1,
+    'digikala.pipelines.SqlitePipeline': 2
 }
 
 IMAGES_STORE = 'images'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+SQLITE_DATABASE_NAME = "digikala.sqlite"
